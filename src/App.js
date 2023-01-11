1cdbfe35b9571c98c3e6ce36bea3d3e6ce5cdb32
Florence Bastaraud <fbastaraud@yahoo.fr>
2023-01-11 19:56:32 +0100

    implemented state and set up the roll dices logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { useState, useEffect } from "react"
+import { nanoid } from "nanoid"
 
+import Dice from "./components/Dice"
 
 
 function App() {
+  const [dices, setDices] = useState(generateRandomDices())
+
+  function generateRandomDices(){
+    const arrayOfDices = []
+      for(let numOfDices = 10; numOfDices > 0; numOfDices--){
+        arrayOfDices.push(newDice())
+      }
+      return arrayOfDices
+  }
+
+  function newDice(){
+    return {id: nanoid(), diceNum: Math.ceil(Math.random() * 6), isKept: false}
+  }
+  
+  useEffect(() => {
+    console.log(dices)
+  }, [dices])
+
+
+  function rollDices(e){
+    e.preventDefault()
+    setDices(generateRandomDices())
+  }
+
+  const dicesElements = dices.map(dice => <Dice key={dice.id} diceNum={dice.diceNum}/>)
+ 
+
   return (
     <main className="container">
       <h1 className="title">Jeu de lancer de dés</h1>
@@ -14,43 +44,15 @@ function App() {
             <li>Appuyez sur les dés qui ont le chiffre que vous venez de choisir.</li>
             <li>Une fois fait, relancez les dés pour générer d'autres chiffres pour les autres dés non séléctionnés dans l'espoir de tomber sur votre chiffre prélectionné.</li>
             <li>Si votre chiffre apparait sur un ou plusieurs des dés après le lancer, appuyez sur ces dés-là et relancez les dés.</li>
-            <li>La partie se termine lorsque tous les dés affichent la même valeur <span class="material-symbols-outlined" style={{color: "orange"}}>celebration</span>.</li>
+            <li>La partie se termine lorsque tous les dés affichent la même valeur <span className="material-symbols-outlined" style={{color: "orange"}}>celebration</span>.</li>
           </ul>
         </div>
         <div className="game-container">
           <div className="dices">
-            <div className="dice">
-              <h2 className="dice-num">1</h2>
-            </div>
-            <div className="dice">
-              <h2 className="dice-num">2</h2>
-            </div>
-            <div className="dice">
-              <h2 className="dice-num">3</h2>
-            </div>
-            <div className="dice">
-              <h2 className="dice-num">4</h2>
-            </div>
-            <div className="dice">
-              <h2 className="dice-num">5</h2>
-            </div>
-            <div className="dice">
-              <h2 className="dice-num">2</h2>
-            </div>
-            <div className="dice">
-              <h2 className="dice-num">3</h2>
-            </div>
-            <div className="dice">
-              <h2 className="dice-num">4</h2>
-            </div>
-            <div className="dice">
-              <h2 className="dice-num">5</h2>
-            </div>
-            <div className="dice">
-              <h2 className="dice-num">6</h2>
-            </div>
+            {dicesElements}
           </div>
-          <button className="roll-dices-btn">Lancer les dés</button>
+          <button
+            className="roll-dices-btn" onClick={rollDices}>Lancer les dés</button>
         </div>
       </section>
     </main>
---
+++ src/components/Dice.js
@@ -0,0 +1,12 @@
+
+export default function Dice({diceNum}){
+
+
+  return (
+
+    <div className="dice">
+      <h2 className="dice-num">{diceNum}</h2>
+    </div>
+
+  )
+}
